4b65cf63728229f2d1e8eca8635c6ec59209bd8a
Unknown <shmeli10@gmail.com>
2017-07-14 19:00:38 +0300

    Added PublicationCreateInterface. Added createPublication() method...

---
+++ app/src/main/java/com/androiditgroup/loclook/v2/interfaces/PublicationCreateInterface.java
@@ -0,0 +1,12 @@
+package com.androiditgroup.loclook.v2.interfaces;
+
+/**
+ * Created by Serghei Ostrovschi on 7/14/17.
+ */
+
+public interface PublicationCreateInterface {
+
+    void onPublicationCreateSuccess();
+
+    void onPublicationCreateError(String error);
+}
